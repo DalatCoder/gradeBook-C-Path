c5f0dd0c1daba7a5bd704e0ef3ca4dee239133e9
DalatCoder <hieuntctk42@gmail.com>
2019-12-19 19:32:13 +0700

    Throw and catch exception

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -53,7 +53,7 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invalid value!");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -15,8 +15,23 @@ namespace GradeBook
 
                 if (input == "q") break;
 
-                var grade = double.Parse(input);
-                book.AddGrade(grade);
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("------------------");
+                }
             }
 
 
