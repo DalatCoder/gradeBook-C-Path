4bf0460b39c56b347f62955b0224737f6d159391
DalatCoder <hieuntctk42@gmail.com>
2019-12-21 18:10:06 +0700

    Add constructor

--- src/GradeBook/NameObject.cs
+++ src/GradeBook/NameObject.cs
@@ -2,6 +2,11 @@ namespace GradeBook
 {
     public class NameObject
     {
+        public NameObject(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; set; }
     }
 }
