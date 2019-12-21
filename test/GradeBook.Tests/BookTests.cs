7d50aa6eee071c25786e23e713c368d95fca40e6
DalatCoder <hieuntctk42@gmail.com>
2019-12-21 18:11:02 +0700

    Fix bugs

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -9,7 +9,7 @@ namespace GradeBook.Tests
         public void BookCalculatesAnAverageGrade()
         {
             // arrange
-            var book = new Book();
+            var book = new Book("New Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.3);
